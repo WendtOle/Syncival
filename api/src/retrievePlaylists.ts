94015d58995eefcc3dd415f3747c9d8a3f059154
Olee <olee@Oles-Air.fritz.box>
2023-07-04 11:18:13 +0200

    Move code to api directory

--- api/src/retrievePlaylists.ts
+++ api/src/retrievePlaylists.ts
@@ -1,15 +1,15 @@
 import { spotifyApi } from "./getSpotifyApi";
 import { getAllItems } from "./utils";
-//const {getUserId} = require('./getUserId.ts');
+const {getUserId} = require('./getUserId.ts');
 require('dotenv').config();
 
 type Playlist = {owner: {id: string}, id: string, name: string}
 
 export const getPlayListNames = async (): Promise<Array<Omit<Playlist, 'owner'>>> => {
     try {
-    //const userId = await getUserId()
+    const userId = await getUserId()
     const playlists = await getAllItems(0, [], spotifyApi.getUserPlaylists.bind(spotifyApi))
-    const ownPlaylists =  playlists//.filter(playlist => playlist.owner.id === userId);
+    const ownPlaylists =  playlists.filter((playlist: Playlist) => playlist.owner.id === userId);
     console.log(`Found ${ownPlaylists.length} playlists.`)
     return ownPlaylists;
     } catch (err: any) {
