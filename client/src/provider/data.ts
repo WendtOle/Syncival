582a24a146ed59f98a885fa7c1d94ae1b19a8269
Olee <olee@Oles-Air.fritz.box>
2023-07-30 14:17:32 +0200

    Handle fusion artists file differently

--- client/src/provider/data.ts
+++ client/src/provider/data.ts
@@ -3,7 +3,5 @@ import { backendUrl } from "../state/loadEnvVariables"
 export const getData = async () :Promise<Record<string,string[]>> => {
     const response = await fetch(`${backendUrl}/data`)
     const data = await response.json()
-    const parsed = Object.entries(data).reduce((acc, [key, value]) => ({...acc, [key]: JSON.parse(value as string)}), {})
-    console.log(parsed)
-    return parsed
+    return data
 }
