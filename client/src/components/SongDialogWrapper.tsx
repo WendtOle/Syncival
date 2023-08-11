106e26b1621d470ecfb393c65d23ee8e7d0714dd
Olee <olee@Oles-Air.fritz.box>
2023-08-11 23:20:50 +0200

    Fix styling of spotify iframe

--- client/src/components/SongDialogWrapper.tsx
+++ client/src/components/SongDialogWrapper.tsx
@@ -1,15 +1,34 @@
-import { Dialog } from "@mui/material"
-import { useState } from "react"
+import { Dialog } from "@mui/material";
+import { useState } from "react";
 
-export const SongDialogWrapper = ({children, id}: {id: string, children: (onClick: () => void) => any}) => {
-    const [open, setOpen] = useState(false)
-    return (
-        <>
-        {children(() => setOpen(true))}
-        <Dialog open={open} onClose={() => setOpen(false)}>
-        <iframe title={`spotify-snippet-player-${id}`} src={`https://open.spotify.com/embed/track/${id}?utm_source=generator`} width="100%" height="352"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-
-        </Dialog>
-        </>
-    )
-}
+export const SongDialogWrapper = ({
+  children,
+  id,
+}: {
+  id: string;
+  children: (onClick: () => void) => any;
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      {children(() => setOpen(true))}
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        PaperProps={{
+          sx: { backgroundColor: "transparent", borderRadius: "12px" },
+        }}
+      >
+        <iframe
+          title={`spotify-snippet-player-${id}`}
+          src={`https://open.spotify.com/embed/track/${id}?utm_source=generator`}
+          width="100%"
+          height="352"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          style={{ borderWidth: 0 }}
+        ></iframe>
+      </Dialog>
+    </>
+  );
+};
