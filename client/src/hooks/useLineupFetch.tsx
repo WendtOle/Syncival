1e09c26cbc135dc46c70bb2c6a976616b89a34c4
Olee <olee@Oles-Air.fritz.box>
2023-08-12 10:08:03 +0200

    Use useEffect hook in useLineupFetch

--- client/src/hooks/useLineupFetch.tsx
+++ client/src/hooks/useLineupFetch.tsx
@@ -3,35 +3,42 @@ import { lineupFetchedAtom } from "../state/fetch";
 import { getLineups } from "../provider/lineups";
 import { lineupsAtom } from "../state/lineups";
 import { selectedLineupKeyAtom } from "../state/main";
+import { useEffect } from "react";
 
 export const useLineupFetch = async () => {
-  const [lineupFetched, setLineupFetched] = useAtom(lineupFetchedAtom);
   const setLineups = useSetAtom(lineupsAtom);
   const setSelectedLineupKey = useSetAtom(selectedLineupKeyAtom);
-  if (lineupFetched) {
-    return;
-  }
-  const fetchedLineups = await getLineups();
-  setLineups((cur) => {
-    const modified = cur.map((lineup) => {
-      const fetchedLineup = fetchedLineups.find(
-        ({ key }) => key === lineup.key,
-      );
-      if (!fetchedLineup) {
-        return lineup;
-      }
-      return {
-        ...lineup,
-        ...fetchedLineup,
-      };
-    });
-    return [
-      ...modified,
-      ...fetchedLineups.filter(
-        ({ key }) => !cur.map(({ key }) => key).includes(key),
-      ),
-    ];
-  });
-  setSelectedLineupKey(fetchedLineups[0].key);
-  setLineupFetched(true);
+  const [lineupFetched, setLineupFetched] = useAtom(lineupFetchedAtom);
+
+  useEffect(() => {
+    if (lineupFetched) {
+      return;
+    }
+    const something = async () => {
+      const fetchedLineups = await getLineups();
+      setLineups((cur) => {
+        const modified = cur.map((lineup) => {
+          const fetchedLineup = fetchedLineups.find(
+            ({ key }) => key === lineup.key,
+          );
+          if (!fetchedLineup) {
+            return lineup;
+          }
+          return {
+            ...lineup,
+            ...fetchedLineup,
+          };
+        });
+        return [
+          ...modified,
+          ...fetchedLineups.filter(
+            ({ key }) => !cur.map(({ key }) => key).includes(key),
+          ),
+        ];
+      });
+      setSelectedLineupKey(fetchedLineups[0].key);
+      setLineupFetched(true);
+    }
+    something()
+  }, []);
 };
