26c72c3532c53c9b4afc08e0cc07922e3cdac928
Olee <olee@Oles-Air.fritz.box>
2023-08-20 14:02:43 +0200

    [#62] Add close button to spotify player dialog

--- client/src/components/SpotifyIFrameWrapper.tsx
+++ client/src/components/SpotifyIFrameWrapper.tsx
@@ -1,5 +1,6 @@
-import { Dialog } from "@mui/material";
+import { Dialog, DialogTitle, IconButton } from "@mui/material";
 import { useState } from "react";
+import { CloseIcon } from "./Icons";
 
 export const SpotifyIFrameWrapper = ({
   children,
@@ -8,27 +9,57 @@ export const SpotifyIFrameWrapper = ({
 }: {
   id: string;
   children: (onClick: () => void) => any;
-  type: "track" | "artist" | "playlist";
+  type: "artist" | "playlist";
 }) => {
   const [open, setOpen] = useState(false);
+
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+
   return (
     <>
-      {children(() => setOpen(true))}
+      {children(onOpen)}
       <Dialog
         open={open}
-        onClose={(e: any) => {
-          e.stopPropagation();
-          setOpen(false);
-        }}
+        onClose={onClose}
         PaperProps={{
-          sx: { backgroundColor: "transparent", borderRadius: "12px" },
+          sx: {
+            backgroundColor: "rgb(40,40,40)",
+            borderRadius: 2,
+            paddingLeft: 1,
+            paddingBottom: 2,
+            height: "75vh",
+            width: "85vw",
+            maxWidth: "800px",
+            maxHeight: "600px",
+          },
         }}
       >
+        <DialogTitle
+          sx={{
+            justifyContent: "end",
+            "&.MuiDialogTitle-root": { display: "flex", padding: 0 },
+          }}
+        >
+          <IconButton
+            sx={{
+              paddingRight: 2,
+              paddingTop: 2,
+              paddingBottom: 0,
+              color: "white",
+              "&:hover": { color: "green" },
+            }}
+            onClick={onClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
         <iframe
           title={`spotify-snippet-player-${id}`}
-          src={`https://open.spotify.com/embed/${type}/${id}?utm_source=generator`}
+          id="spotify-iframe"
+          src={`https://open.spotify.com/embed/${type}/${id}?utm_source=generator&theme=0`}
           width="100%"
-          height="352"
+          height="100%"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
           style={{ borderWidth: 0 }}
