cc5029a84aaea86f6b0655efce6b59e20fbfad2b
Olee <olee@Oles-Air.fritz.box>
2023-08-20 15:40:36 +0200

    [#35] Add spotify logo to app bar

---
+++ client/src/SpotifyLogo.tsx
@@ -0,0 +1,23 @@
+const SPOTIFY_LOGO_MIN_SIZE = 21;
+export const SpotifyLogo = ({
+  variant,
+  size: nullableSize,
+}: {
+  variant?: "black" | "white";
+  size?: number;
+}) => {
+  const path =
+    !variant || variant === "white"
+      ? "Spotify_Icon_RGB_White.png"
+      : "Spotify_Icon_RGB_Black.png";
+  const size = Math.max(nullableSize ?? 0, SPOTIFY_LOGO_MIN_SIZE);
+  return (
+    <img
+      src={path}
+      width={`${size}px`}
+      height={`${size}px`}
+      alt="spotify logo"
+      style={{ padding: `${size / 2}px` }}
+    />
+  );
+};
