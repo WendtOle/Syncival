3e53e61539c86059e7891cc46f4fc0442d4eb01c
Olee <olee@Oles-Air.fritz.box>
2024-01-06 11:04:10 +0100

    Extract `getMySavedTracks()` and `getPlaylistTracks()`

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,7 +2,6 @@
 import { getTokens } from './authorizeApi';
 
 const express = require('express');
-import {spotifyApi} from "./getSpotifyApi";
 import { getUserId } from './getUserId';
 const url = require('url');
 const querystring = require('querystring');
@@ -21,6 +20,8 @@ import {createPlaylist as providerCreatePlaylist} from './createPlaylist'
 import { addTracksToPlaylist } from './addTracksToPlaylist';
 import { timeStamp } from './getCurrentTimeStamp';
 import { replaceTracksInPlaylist } from './replaceTracksInPlaylist';
+import { getMySavedTracks } from './provider/getMySavedTracks';
+import { getPlaylistTracks } from './provider/getPlaylistTracks';
 
 
 const setCors = (req: any, res: any) => {
@@ -134,16 +135,15 @@ app.get('/tracks', async (req: any, res: any) => {
     const { query } = url.parse(req.url);
     const { accessToken, page, playlistId } = querystring.parse(query);
     try {
-        await spotifyApi.setAccessToken(accessToken);
         console.log('/tracks', {playlistId, page})
         const getResponse = () => {
             if (playlistId === 'liked_songs') {
-                return spotifyApi.getMySavedTracks({limit: 50, offset: page * 50})
+                return getMySavedTracks({accessToken, limit: 50, page})
             }
-            return spotifyApi.getPlaylistTracks(playlistId,{limit: 50, offset: page * 50})
+            return getPlaylistTracks({playlistId, accessToken, limit: 50, page})
         }
         const response = await getResponse()
-        const trackData: Something = response.body.items.map(({track}) => track).filter(track => track !== null).map(({id, name, artists, album}: any) => {
+        const trackData: Something = response.map(({track}) => track).filter(track => track !== null).map(({id, name, artists, album}: any) => {
             const image = album.images.reduce((smallest: any, image: any) => {
                 if (image.height < smallest.height) return image
                 return smallest
