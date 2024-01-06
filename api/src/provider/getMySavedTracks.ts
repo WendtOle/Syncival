db48fd54a35a0705e491dac005a0c9bf3e003892
Olee <olee@Oles-Air.fritz.box>
2024-01-06 11:04:10 +0100

    Extract `getMySavedTracks()` and `getPlaylistTracks()`

---
+++ api/src/provider/getMySavedTracks.ts
@@ -0,0 +1,13 @@
+import { spotifyApi } from "../getSpotifyApi";
+
+interface GetMySavedTracksProps {
+    accessToken: string, 
+    limit: number, 
+    page: number
+}
+
+export const getMySavedTracks = async ({accessToken, limit, page}: GetMySavedTracksProps): Promise<SpotifyApi.SavedTrackObject[]> => {
+    spotifyApi.setAccessToken(accessToken);
+    const data = await spotifyApi.getMySavedTracks({limit, offset: page * 50});
+    return data.body.items;
+}
