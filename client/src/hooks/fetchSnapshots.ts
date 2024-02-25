12b5d7002e134cb087de3f241b554adcad0242e1
Olee <olee@Oles-Air.fritz.box>
2024-02-25 18:09:24 +0100

    Load snapshots continuisly

--- client/src/hooks/fetchSnapshots.ts
+++ client/src/hooks/fetchSnapshots.ts
@@ -5,51 +5,42 @@ interface FetchSnapshotProps {
   existingSnapshots: Record<string, Track[]>;
   playlistInfo: Record<string, PlaylistInformation>;
   accessToken: () => string;
+  addSnapshot: (newSnapShot: Record<string, Track[]>) => void;
+  removeSnapshots: (ids: string[]) => void;
 }
 
 export const fetchSnapshots = async ({
   existingSnapshots,
   accessToken,
   playlistInfo,
+  addSnapshot,
+  removeSnapshots,
 }: FetchSnapshotProps) => {
   const existingSnapshotIds = Object.keys(existingSnapshots);
-  type someType = [string[], PlaylistInformation[]];
-  const init: someType = [[], []];
-  const [toKeep, toFetch] = Object.values(playlistInfo).reduce(
-    ([toKeep, toFetch], curr): someType => {
-      const snapShotExists = existingSnapshotIds.includes(curr.snapShotId);
-      if (snapShotExists) {
-        return [[...toKeep, curr.snapShotId], toFetch];
-      } else {
-        return [toKeep, [...toFetch, curr]];
-      }
-    },
-    init
+  const toFetch = Object.values(playlistInfo).filter(
+    ({ snapShotId }) => !existingSnapshotIds.includes(snapShotId)
   );
 
   console.log({
     total: Object.values(playlistInfo).length,
     toFetch: toFetch.length,
-    toKeep: toKeep.length,
   });
 
   console.log("toFetch", toFetch.map(({ name }) => name).join(", "));
 
-  const newSnapshots = await Promise.all(
-    toFetch.map(async ({ snapShotId, id }) => {
-      const response = await fetchPlaylistSongs(accessToken, id);
-      return [snapShotId, response] as const;
-    })
+  toFetch.forEach(async ({ snapShotId, id }) => {
+    const response = await fetchPlaylistSongs(accessToken, id);
+    addSnapshot({ [snapShotId]: response });
+  });
+
+  const snapShotsIdsToKeep = Object.values(playlistInfo).map(
+    ({ snapShotId }) => snapShotId
   );
-  return [
-    ...toKeep.map(
-      (snapshotId) => [snapshotId, existingSnapshots[snapshotId]] as const
-    ),
-    ...newSnapshots,
-  ].reduce(
-    (acc, [snapshotId, tracks]) => ({ ...acc, [snapshotId]: tracks }),
-    {}
+  const toDelete = existingSnapshotIds.filter(
+    (id) => !snapShotsIdsToKeep.includes(id)
   );
+  console.log("toDelete", toDelete);
+  removeSnapshots(toDelete);
 };
 
 const fetchPlaylistSongs = async (
