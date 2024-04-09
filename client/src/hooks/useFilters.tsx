374b1d7fc71b0b124c74c323e52f078bdd715512
Olee <olee@Oles-Air.fritz.box>
2024-04-09 22:05:25 +0200

    Refactor useFilters.tsx

--- client/src/hooks/useFilters.tsx
+++ client/src/hooks/useFilters.tsx
@@ -7,6 +7,7 @@ import {
   ArtistFilterOption,
   FilterGroupOption,
   GroupableFilterOption,
+  OrFilterProps,
 } from "../types/filter";
 import { filterStateMachine } from "../util/filterStateMachine";
 import { getOnClick } from "../util/getOnClick";
@@ -23,40 +24,45 @@ export const useFilters = (): UseFiltersReturnValue => {
   const [artistFilter, setArtistFilter] = useAtom(artistsFilterAtom);
   const result = useArtists();
 
-  const key =
-    typeof artistFilter === "object" ? artistFilter.filter : artistFilter;
-
-  const additionalFilter =
+  const additionalFilter: OrFilterProps[] = (
     typeof artistFilter === "object"
       ? filterGroupMapping[artistFilter.filter].filter(
           (filter) => !artistFilter.items.includes(filter)
         )
-      : [];
+      : []
+  ).map((filter) => ({
+    current: filter,
+    append: groupableFilterMapping[filter],
+  }));
 
-  const filterToShow = [
-    ...filterStateMachine[key],
-    ...additionalFilter.map((filter) => ({
-      current: filter,
-      append: groupableFilterMapping[filter],
-    })),
-  ]
+  const state =
+    typeof artistFilter === "object" ? artistFilter.filter : artistFilter;
+
+  const filterOptions = [...filterStateMachine[state], ...additionalFilter];
+
+  const filter = filterOptions
     .filter((entry) => {
       return (
         !("append" in entry) ||
         (result !== undefined && result(entry.current).length > 0)
       );
     })
-    .map((entry) => ({
-      current: entry.current,
-      onClick: getOnClick(entry, artistFilter, setArtistFilter),
-      selected: "next" in entry && entry.current !== entry.next,
-      label:
+    .map((entry) => {
+      const isSelected = "next" in entry && entry.current !== entry.next;
+      const label =
         typeof artistFilter === "object" &&
         artistFilter.filter === entry.current
           ? artistFilter.items.map((item) => filterNames[item]).join(" & ")
-          : filterNames[entry.current as ArtistFilterOption],
-    }))
+          : filterNames[entry.current as ArtistFilterOption];
+      const onClick = getOnClick(entry, artistFilter, setArtistFilter);
+      return {
+        current: entry.current,
+        onClick,
+        selected: isSelected,
+        label,
+      };
+    })
     .sort(({ selected }) => (selected ? -1 : 1));
 
-  return filterToShow;
+  return filter;
 };
