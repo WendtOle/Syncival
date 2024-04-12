ae59269b8ac09796637174305ba74290d78cb169
Olee <olee@Oles-Air.fritz.box>
2024-04-12 22:16:04 +0200

    Add button for reloading data

--- client/src/components/FestivalSelectionScreen.tsx
+++ client/src/components/FestivalSelectionScreen.tsx
@@ -4,9 +4,20 @@ import { useQuery } from "@tanstack/react-query";
 import { lineupsQuery } from "../provider/queries";
 import { FestivalItem } from "./FestivalItem";
 import { Festival } from "../types/festival";
+import {
+  Avatar,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
 export const FestivalSelectionScreen = () => {
   const { data: festivals } = useQuery<Festival[]>(lineupsQuery);
+
+  const clearLocalStorage = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
 
   return (
     <div>
@@ -15,6 +26,25 @@ export const FestivalSelectionScreen = () => {
         {festivals?.map((festival) => (
           <FestivalItem key={festival.key} festival={festival} />
         ))}
+        <ListItemButton onClick={clearLocalStorage}>
+          <ListItemAvatar>
+            <Avatar
+              style={{
+                height: 54,
+                width: 54,
+                background: "#fff9c4",
+                color: "gray",
+              }}
+              variant="square"
+            >
+              {" "}
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={"Clear and reload data"}
+            sx={{ marginLeft: 2 }}
+          />
+        </ListItemButton>
       </List>
     </div>
   );
