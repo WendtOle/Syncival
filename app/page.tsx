93d394e7f9c296bdc832cf0fd55c9a1847beb085
Olee <olee@Oles-Air.fritz.box>
2024-05-16 20:17:27 +0200

    Style home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ import { Festival, festivalNames } from "./data/festivalInformation";
 
 export default async function Home() {
   return (
-    <ul>
-      {Object.values(Festival).map((key) => (
-        <li key={key}>
-          <Link href={`./${key}`}>{festivalNames[key]}</Link>
-        </li>
-      ))}
-    </ul>
+    <div className="max-w-xl flex justify-center text-xl mt-8">
+      <ul>
+        {Object.values(Festival).map((key) => (
+          <li key={key} className="mb-2 px-4 p-2 border rounded">
+            <Link className="block" href={`./${key}`}>
+              {festivalNames[key]}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
