42ff8097c7c8c2059ae75b8ca53ee62bebdd34b1
Olee <olee@Oles-Air.fritz.box>
2024-05-16 20:45:13 +0200

    Extract Item component

---
+++ app/Item.tsx
@@ -0,0 +1,14 @@
+export default async function Item({
+  title,
+  image,
+}: {
+  title: string;
+  image: any;
+}) {
+  return (
+    <div className="flex items-center mx-2 mb-1">
+      {image}
+      <p className="ml-4"> {title}</p>
+    </div>
+  );
+}
--- app/[festivalKey]/ArtistItem.tsx
+++ app/[festivalKey]/ArtistItem.tsx
@@ -1,22 +1,25 @@
+import Item from "../Item";
 import { Artist, isSpotifyArtist } from "../artist";
 
 export default async function ArtistComponent({ artist }: { artist: Artist }) {
   if (!isSpotifyArtist(artist)) {
     return (
-      <div className="flex items-center mx-2 mb-1">
-        <div className="w-16 h-16 flex justify-center items-center bg-gray-100">
-          <p className="font-medium text-lg">?</p>
-        </div>
-        <p className="ml-4"> {artist.name}</p>
-      </div>
+      <Item
+        title={artist.name}
+        image={
+          <div className="w-16 h-16 flex justify-center items-center bg-gray-100">
+            <p className="font-medium text-lg">?</p>
+          </div>
+        }
+      />
     );
   }
   const image: SpotifyApi.ImageObject | undefined =
     artist.images[artist.images.length - 1];
   return (
-    <div className="flex items-center mx-2 mb-1">
-      <img src={image?.url ?? ""} width={64} height={64} />
-      <p className="ml-4">{artist.name}</p>
-    </div>
+    <Item
+      title={artist.name}
+      image={<img src={image?.url ?? ""} width={64} height={64} />}
+    />
   );
 }
