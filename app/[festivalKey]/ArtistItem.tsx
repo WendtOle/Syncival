84c521045d1ecca7a360ebfc1f06cc45ec9494ac
Olee <olee@Oles-Air.fritz.box>
2024-05-16 20:35:21 +0200

    Style ArtistItem

--- app/[festivalKey]/ArtistItem.tsx
+++ app/[festivalKey]/ArtistItem.tsx
@@ -3,20 +3,20 @@ import { Artist, isSpotifyArtist } from "../artist";
 export default async function ArtistComponent({ artist }: { artist: Artist }) {
   if (!isSpotifyArtist(artist)) {
     return (
-      <div className="flex">
-        <div className="w-16 h-16 content-center">
-          <p>{artist.name?.[0]}</p>
+      <div className="flex items-center mx-2 mb-1">
+        <div className="w-16 h-16 flex justify-center items-center bg-gray-100">
+          <p className="font-medium text-lg">?</p>
         </div>
-        <p>{artist.name} (not found on spotify)</p>
+        <p className="ml-4"> {artist.name}</p>
       </div>
     );
   }
   const image: SpotifyApi.ImageObject | undefined =
     artist.images[artist.images.length - 1];
   return (
-    <div className="flex">
-      <img src={image?.url ?? ""} width={64} />
-      <p>{artist.name}</p>
+    <div className="flex items-center mx-2 mb-1">
+      <img src={image?.url ?? ""} width={64} height={64} />
+      <p className="ml-4">{artist.name}</p>
     </div>
   );
 }
