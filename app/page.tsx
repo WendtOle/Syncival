ce63ec7d0a01345857a378fa2a2465ea5fe70040
Olee <olee@Oles-Air.fritz.box>
2024-05-16 20:49:30 +0200

    Restyle lineup selection screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 import Link from "next/link";
 import { Festival, festivalNames } from "./data/festivalInformation";
+import Item from "./Item";
 
 export default async function Home() {
   return (
-    <div className="max-w-xl flex justify-center text-xl mt-8">
-      <ul>
-        {Object.values(Festival).map((key) => (
-          <li key={key} className="mb-2 px-4 p-2 border rounded">
-            <Link className="block" href={`./${key}`}>
-              {festivalNames[key]}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="mt-2">
+      {Object.values(Festival).map((key) => {
+        const name = festivalNames[key];
+        return (
+          <Link key={key} href={`/${key}`}>
+            <Item
+              key={key}
+              title={name}
+              image={
+                <div className="w-16 h-16 flex justify-center items-center bg-gray-100">
+                  <p className="font-medium text-lg">{name[0]}</p>
+                </div>
+              }
+            />
+          </Link>
+        );
+      })}
     </div>
   );
 }
