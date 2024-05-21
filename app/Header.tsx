79f3ee3757ba696ee47257850e2c6fed28fee338
Olee <olee@Oles-Air.fritz.box>
2024-05-21 19:11:53 +0200

    Add spotify logo to header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default async function Header({ title }: { title: string }) {
   return (
-    <div className="flex justify-center p-4 sticky top-0 bg-white">
+    <div className="flex justify-between p-4 sticky top-0 bg-white items-center">
       <h1 className="text-xl">{title}</h1>
+      <Image src="/Spotify_Logo_RGB_Black.png" alt="" width={100} height={50} />
     </div>
   );
 }
