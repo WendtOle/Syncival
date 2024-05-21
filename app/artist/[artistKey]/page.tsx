943cb539689218a2f4d8f086cd8547e9fd18047b
Olee <olee@Oles-Air.fritz.box>
2024-05-21 18:53:58 +0200

    fixup - spotify iframe background

--- app/artist/[artistKey]/page.tsx
+++ app/artist/[artistKey]/page.tsx
@@ -6,7 +6,7 @@ export default async function Artist(params: {
   const { artistKey } = params.params;
 
   return (
-    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-spotify-iframe-background sm:bg-white">
       <div className="w-full h-full sm:w-3/4 sm:h-3/4">
         <SpotifyIframe artistKey={artistKey} />
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "spotify-iframe-background": "#282828",
+      },
     },
   },
   plugins: [],
