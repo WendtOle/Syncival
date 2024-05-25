0d5332bf30b9fe873ba78c7b3863e30003aa25cc
Olee <olee@Oles-Air.fritz.box>
2024-05-25 15:24:15 +0200

    Add back button on lineup pages

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Header({
   title,
@@ -10,14 +11,17 @@ export default async function Header({
   return (
     <div className="sticky top-0 p-4  bg-white flex flex-col space-y-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-xl">{title}</h1>
+        <div className="flex flex-row items-center space-x-2">
+          <Link href="/" className="w-4 h-4"><img src="/back-icon.svg" /></Link>
+          <h1 className="text-xl">{title}</h1>
+        </div>
         <Image
           src="/Spotify_Logo_RGB_Black.png"
           alt=""
           width={100}
           height={50}
-        />
-      </div>
+          />
+        </div>
       {children}
     </div>
   );
