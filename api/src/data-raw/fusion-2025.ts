43f7ce5302856337bd729c2b16656e401e86af9e
Ole <ole@Mac.fritz.box>
2025-06-23 15:57:32 +0200

    Use updated chunk file

--- api/src/data-raw/fusion-2025.ts
+++ api/src/data-raw/fusion-2025.ts
@@ -2,7 +2,7 @@ import { writeFileSync } from 'fs';
 import fetch from 'node-fetch'; 
 
 //the web app from the fusion loads a chung js file, retrieve and parse it!
-const URL = "https://timetable.fusion-festival.de/static/js/346.003d780e.chunk.js"
+const URL = "https://timetable.fusion-festival.de/static/js/346.42b2cd90.chunk.js"
 
 const fetchAndExtractJSON = async () => {
   const response = await fetch(URL);
